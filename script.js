2a3210ccb3baa62fdda06f39aadcc3373ebc4f79
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-11-03 19:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function volume_sphere() {
     //Write your code here
-  
+
+	document.getElementById("submit").addEventListener('click',(event)=>{
+		event.preventDefault();
+		 let radius= document.getElementById("radius").value;
+		let volume ;
+		if (isNaN(radius) || radius < 0) {
+            volume = 'Invalid input';
+        } else {
+            radius = parseFloat(radius); // Convert to a number
+            volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+			volume=volume.toFixed(4);
+        }
+		
+		 document.getElementById("volume").value=volume;
+	})
+ 
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
